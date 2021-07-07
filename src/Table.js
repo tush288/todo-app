1b5ca2a4a65e6cc982c80e7b03ede1c8aed41c42
tush288 <tushar_verma@excellencetechnologies.in>
2021-07-07 18:39:26 +0530

    changing the ui

--- src/Table.js
+++ src/Table.js
@@ -6,6 +6,7 @@ import moment from "moment";
 import DateTimePicker from "react-datetime-picker";
 import { BsFillClockFill } from "react-icons/bs";
 import { List } from "rc-field-form";
+import { Modal } from "react-bootstrap";
 
 function Table(props) {
   // const [value, onChange] = useState(new Date());
@@ -15,73 +16,86 @@ function Table(props) {
 
   return (
     <div>
-      <table className="table table table-dark table-hover">
-        <thead>
-          <tr>
-            <th scope="col">Status</th>
-            <th scope="col">Name</th>
-            <th scope="col">Time</th>
-            <th scope="col">Action</th>
-            <th scope="col">
-              <Button onClick={props.handleSort}>sort</Button>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.data.map((item, index) => (
-            <tr key={index}>
-              <td>
-                <Checkbox
-                  className="app__list__checkbox"
-                  type="checkbox"
-                  checked={props.data[index].status}
-                  onChange={(e) => props.onChangeCheckBox(e, index)}
-                />
-              </td>
-              <td>
-                {props.activeIndex !== index ? (
-                  item.name
-                ) : (
-                  <Input
-                    value={item.name}
-                    onChange={(e) => props.onChangeTaskName(e, index)}
-                    onPressEnter={(e) => props.onEnterTaskName(e, index)}
-                  />
-                )}
-              </td>
-              <td>
-                {/* {moment(item.date).format("Do MMM YY,h:mm a")} */}
+      <Modal.Dialog>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
 
-                <DateTimePicker
-                  value={item.date}
-                  onChange={(date) => props.onDateChange(index, date)}
-                  calendarIcon={<BsFillClockFill />}
-                  format="y-MM-dd h:mm a"
-                ></DateTimePicker>
-              </td>
-              <td>
-                <Button
-                  className="btn btn-warning table_button_edit "
-                  onClick={() => props.onEdit(index)}
-                >
-                  edit
-                </Button>
+        <Modal.Body>
+          <table className="table table table-dark table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Status</th>
+                <th scope="col">Name</th>
+                <th scope="col">Time</th>
+                <th scope="col">Action</th>
+                <th scope="col">
+                  <Button onClick={props.handleSort}>sort</Button>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {props.data.map((item, index) => (
+                <tr key={index}>
+                  <td>
+                    <Checkbox
+                      className="app__list__checkbox"
+                      type="checkbox"
+                      checked={props.data[index].status}
+                      onChange={(e) => props.onChangeCheckBox(e, index)}
+                    />
+                  </td>
+                  <td>
+                    {props.activeIndex !== index ? (
+                      item.name
+                    ) : (
+                      <Input
+                        value={item.name}
+                        onChange={(e) => props.onChangeTaskName(e, index)}
+                        onPressEnter={(e) => props.onEnterTaskName(e, index)}
+                      />
+                    )}
+                  </td>
+                  <td>
+                    {/* {moment(item.date).format("Do MMM YY,h:mm a")} */}
 
-                <Popconfirm
-                  title="Are you sure to delete this task?"
-                  onConfirm={() => props.onDelete(index)}
-                  onCancel={() => {}}
-                  okText="Yes"
-                  cancelText="No"
-                >
-                  <Button className="btn btn-danger">delete</Button>
-                </Popconfirm>
-              </td>
-              <td></td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                    <DateTimePicker
+                      value={item.date}
+                      onChange={(date) => props.onDateChange(index, date)}
+                      calendarIcon={<BsFillClockFill />}
+                      format="y-MM-dd h:mm a"
+                    ></DateTimePicker>
+                  </td>
+                  <td>
+                    <Button
+                      className="btn btn-warning table_button_edit "
+                      onClick={() => props.onEdit(index)}
+                    >
+                      edit
+                    </Button>
+
+                    <Popconfirm
+                      title="Are you sure to delete this task?"
+                      onConfirm={() => props.onDelete(index)}
+                      onCancel={() => {}}
+                      okText="Yes"
+                      cancelText="No"
+                    >
+                      <Button className="btn btn-danger">delete</Button>
+                    </Popconfirm>
+                  </td>
+                  <td></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary">Close</Button>
+          <Button variant="primary">Save changes</Button>
+        </Modal.Footer>
+      </Modal.Dialog>
     </div>
   );
 }
